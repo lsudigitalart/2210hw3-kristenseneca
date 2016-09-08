7560879536efc7cd0b8943959dc7e0af3adbfaaa
Kristen Seneca <kristenseneca@Kristens-MacBook-Pro.local>
2016-09-08 09:44:25 -0500

    done

---
+++ sketch.js
@@ -0,0 +1,106 @@
+        //  Kristen Seneca ©2016 hw3
+
+var x;
+
+function setup() {
+
+
+createCanvas(1400 , 1000)
+background(255);
+x = 20;
+println("my awesome variable =", x , "width =", width, "height =", height);
+
+for(var n = 0; n < 1400; n = n + 30){
+  println("the value n:", n);
+  noStroke();
+  fill(255,0,0,100);
+
+  ellipse(n+10, 10, x);
+  ellipse(n+20, 20, x);
+  ellipse(n+30, 30, x);
+  ellipse(n+40, 40, x)
+  ellipse(n+50, 50, x)
+  ellipse(n+60, 60, x)
+  ellipse(n+70, 70, x)
+  ellipse(n+80, 80, x)
+  ellipse(n+90, 90, x)
+  ellipse(n+100, 100, x)
+  ellipse(n+110, 110, x)
+  ellipse(n+120, 120, x)
+  ellipse(n+130, 130, x)
+  ellipse(n+140, 140, x)
+  ellipse(n+150, 150, x)
+
+
+  noStroke();
+  fill(255,255,0,100);
+
+  ellipse(n+0, 80, x);
+  ellipse(n+10, 90, x);
+  ellipse(n+20, 100, x);
+  ellipse(n+30, 110, x)
+  ellipse(n+40, 120, x)
+  ellipse(n+50, 130, x)
+  ellipse(n+60, 140, x)
+  ellipse(n+70, 150, x)
+  ellipse(n+80, 160, x)
+  ellipse(n+90, 170, x)
+  ellipse(n+100, 180, x)
+  ellipse(n+110, 190, x)
+  ellipse(n+120, 200, x)
+  ellipse(n+130, 210, x)
+  ellipse(n+140, 220, x)
+
+  noStroke();
+  fill(0,255,0,100);
+
+  ellipse(n+0, 230, x);
+  ellipse(n+10, 240, x);
+  ellipse(n+20, 250, x);
+  ellipse(n+30, 260, x)
+  ellipse(n+40, 270, x)
+  ellipse(n+50, 280, x)
+  ellipse(n+60, 290, x)
+  ellipse(n+70, 300, x)
+  ellipse(n+80, 310, x)
+  ellipse(n+90, 320, x)
+  ellipse(n+100, 330, x)
+  ellipse(n+110, 340, x)
+  ellipse(n+120, 350, x)
+  ellipse(n+130, 360, x)
+  ellipse(n+140, 370, x)
+
+  noStroke();
+  fill(0,255,255,100);
+
+  ellipse(n+0, 300, x);
+  ellipse(n+10, 310, x);
+  ellipse(n+20, 320, x);
+  ellipse(n+30, 330, x)
+  ellipse(n+40, 340, x)
+  ellipse(n+50, 350, x)
+  ellipse(n+60, 360, x)
+  ellipse(n+70, 370, x)
+  ellipse(n+80, 380, x)
+  ellipse(n+90, 390, x)
+  ellipse(n+100, 400, x)
+  ellipse(n+110, 410, x)
+  ellipse(n+120, 420, x)
+  ellipse(n+130, 430, x)
+  ellipse(n+140, 440, x)
+
+
+  // line(10, 10, width-5, height-5);
+  // ellipse(10, 10, x);
+  // ellipse(width-5, height-5, x);
+
+
+}
+
+
+}
+
+
+    function draw() {
+
+}
